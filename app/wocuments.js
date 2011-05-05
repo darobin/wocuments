8e1553e7ca757ec2671498ea4dc91c7710458b68
darobin <robin@berjon.com>
2011-05-05 13:50:50 +0200

    trying to work around stupid jar problem; improved example

--- app/wocuments.js
+++ app/wocuments.js
@@ -7,7 +7,10 @@
     
     // modules
     const fp = require("file-picker"),
-          zip = require("zip");
+          zip = require("zip"),
+          url = require("url"),
+          windowOpener = require("open-window");
+          // sandboxWindow = require("chromeless-sandbox-window");
 
     // recent documents list
     // XXX same code can be reused for document models
@@ -59,12 +62,89 @@
     function Wocument () {
     }
     Wocument.prototype = {
+        // jar:file:///Projects/wocuments/wocuments/examples/basic-save-value.woc!/basic-save-value/index.html
         openFromFile:    function (filePath) {
-            alert(filePath);
-            this.zip = zip.open(filePath);
-            alert("contents: " + string);
+            console.log("### opening for " + filePath);
+            var woc = zip.open(filePath);
+            this.zip = woc;
+            console.log("contents:\n\t" + this.zip.allEntryPaths().join("\n\t"));
+            var jarURI = "jar:" + url.fromFilename(filePath) + "!/",
+                baseDir = "";
+            if (!woc.hasEntry("index.html")) {
+                // we assume that everything is in a directory at the root,
+                // so we just take the first part of the first entry path and use that
+                var paths = woc.allEntryPaths();
+                if (!paths.length) throw("Empty Zip archive");
+                baseDir = (/^[^\/]+\//.exec(paths[0]))[0];
+                jarURI += baseDir;
+            }
+            this.baseURI = jarURI;
+            this.baseDir = baseDir;
+            jarURI += "index.html";
+            // alert("jarURI=" + jarURI);
+            this.openWindow(jarURI);
+        },
+        openWindow:    function (uri) {
+            // XXX note that this is built on a temporary hack that involves 
+            // a window that probably isn't properly sandboxed
+            var self = this;
+            var wocWindow = windowOpener.open({
+                url: uri,
+                // XXX set these in a better way
+                width: 600,
+                height: 500,
+                resizable: true ,
+                menubar: false,
+                injectProps: {
+                    console: {
+                        log: function() {
+                            console.log.apply(console, Array.prototype.slice.call(arguments));
+                        }
+                    },
+                    exit: function() {
+                        console.log("window.exit() called...");
+                        wocWindow.close();
+                    },
+                    wocument:   new WocumentObject(this),
+                },
+                onclose:    function () {
+                    console.log("close called");
+                    self.zip.close();
+                },
+            });
+            this.wocWindow = wocWindow;
         },
     };
 
+    function WocumentObject (woc) {
+        this.woc = woc;
+        this.dataDirectory = new DataDirectory(woc.baseDir + "data/", woc);
+    }
+    WocumentObject.prototype = {};
+    function DataDirectory (dir, woc) {
+        console.log("configured DataDirectory with dir=" + dir);
+        this.dir = dir;
+        this.woc = woc;
+    }
+    DataDirectory.prototype = {
+        getFileAsJSON:    function (name, cb) {
+            console.log("getFileAsJSON for name=" + name);
+            var zip = this.woc.zip,
+                entry = this.dir + name;
+            console.log("entry=" + entry);
+            if (zip.hasEntry(entry)) {
+                console.log("entry exists");
+                cb(JSON.parse(zip.entryAsText(entry)));
+                console.log("callback called");
+            }
+            else {
+                console.log("entry does not exist");
+                cb(null);
+            }
+        },
+        saveFileAsJSON:    function (name, object) {
+            this.woc.zip.addEntryFromText(this.dir + name, JSON.stringify(object));
+        },
+    };
 
 })(window, jQuery);
